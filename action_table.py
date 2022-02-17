e648b45d443b2d84b9b4b4925d1b13a3aa85a74c
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-17 14:43:02 +0300

    побаловался с функциями

--- action_table.py
+++ action_table.py
@@ -1,4 +1,19 @@
+
 def output_all(source):
     for i in source:
         print(f'{i} {source[i]["name"]} {source[i]["surname"]} {source[i]["class"]}', end='')
         print()
+
+def sort_class(source,num_class):
+    for i in source:
+        if (source[i]["class"] == str(num_class)):
+            print(f'{i} {source[i]["surname"]} {source[i]["name"]} - {source[i]["class"]}')
+
+def sort_abc(source):
+    result = []
+    for i in source:
+        result.append(f'{source[i]["surname"]} {source[i]["name"]} id - {i}')
+    result.sort()
+    for i in result:
+        print(i)
+
