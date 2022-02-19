e2ba900ac3839304abd1e21b2439ded6bd685b28
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-19 21:26:56 +0300

    Окончательные итоги вечерней сборки)

--- additional.py
+++ additional.py
@@ -9,19 +9,9 @@ def distribution(key_func,source,path):
     elif (key_func == '3'): working_file.del_info(source, path)
     elif (key_func == '4'): modul1.sort(path)
     elif (key_func == '5'): action_table.sort_abc(source)
-    elif(key_func == '6'): return 'Vs'
+    elif (key_func == '6'): print('Всего хорошего!')
     else: print('Введите корректное число')
 
-def init_gl(source_in,path_in):
-    global source
-    source = source_in
-    global path
-    path = path_in
-
-do_it = { '1':action_table.output_all(source),'2':working_file.append_info(source,path),
-            '3':working_file.del_info(source, path),'4':modul1.sort(path), '5':action_table.sort_abc(source)}
-
-def distribution_sl(key_fun):
 
 
 
--- controller.py
+++ controller.py
@@ -1,16 +1,14 @@
 import additional
 import r_w_file
-import working_file
-import action_table
 import view
 
 path_txt = 'base_data.txt'
 data = r_w_file.read_file(f'{path_txt}')
-count = 0
-while(count < 6):
+number = ''
+while(number != '6'):
     number = view.get_request()
-    additional.init_gl(data,path_txt)
-    additional.distribution_sl(number)
+    additional.distribution(number,data,path_txt)
+
 
 
 
