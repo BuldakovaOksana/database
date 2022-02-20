5f49b7e85ef8cb21699a25f7d4b6bf122496187a
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-20 10:31:59 +0300

    сделал главную функцию, добавил выбор файла, с которым будет вестись работа

--- controller.py
+++ controller.py
@@ -1,13 +1,17 @@
-import additional
+import Knot
 import r_w_file
 import view
 
-path_txt = 'base_data.txt'
-data = r_w_file.read_file(f'{path_txt}')
-number = ''
-while(number != '6'):
-    number = view.get_request()
-    additional.distribution(number,data,path_txt)
+def start():
+    path_txt = input('Название файла с которым вы хотите работать: ')
+    data = r_w_file.read_file(f'{path_txt}')
+    number = ''
+    while (number != '6'):
+        number = view.get_request()
+        Knot.init(data, path_txt)
+        Knot.distribution(number)
+        print()
+
 
 
 
