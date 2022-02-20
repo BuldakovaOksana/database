8aacca04799e3f63a34b7b5a8abb017208e966a6
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-20 10:30:48 +0300

    Перенёс в данный модуль функции с добавлением и удалением пользователя

--- r_w_file.py
+++ r_w_file.py
@@ -8,6 +8,7 @@ def write_file(source, path):
         file.writelines(f'{sup_str} \n')
         sup_str = ''
     file.close()
+    return source
 
 
 def read_file(path):
@@ -23,3 +24,23 @@ def read_file(path):
         file.writelines('')
         file.close()
     return result
+
+
+def append_info(source, path):
+    new_name = input('Введите Имя ')
+    new_surname = input('Введите Фамилию ')
+    new_class = input('Введите номер класса ')
+    if os.stat(path).st_size == 0: index = 0
+    else:
+        index_list = list(map(lambda el:int(el),source))
+        index = int(max(index_list))+1
+    source[index] = {'name':new_name, 'surname': new_surname, 'class' : new_class}
+    write_file(source, path)
+    return source
+
+
+def del_info(source, path):
+    index = input('Введите индекс пользователя, которого необходимо удалить: ')
+    del source[index]
+    write_file(source, path)
+    return source
