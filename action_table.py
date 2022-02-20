91ce35e9a34568c6928e098e2a52cf7fdd4c07b7
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-20 10:27:00 +0300

    Перенёс метод с сортировкой по классу в данный модуль, а также сделал все функции возвращаемыми

--- action_table.py
+++ action_table.py
@@ -4,17 +4,34 @@ def output_all(source):
         print(f'{i} {source[i]["name"]} {source[i]["surname"]} {source[i]["class"]}', end='')
         print()
 
-def sort_class(source):
-    num_class = input('Введите номер класса: ')
-    for i in source:
-        if (source[i]["class"] == str(num_class)):
-            print(f'{i} {source[i]["surname"]} {source[i]["name"]} - {source[i]["class"]}')
+def sort_num_class(data):
+    a = input('Введите номер класса: ')
+    sup_str = ''
+    file = open(data, 'r')
+    for line in file:
+        arr = line.split()
+        if arr[3] == a:
+            sup_str += (f'{arr[0]} {arr[1]} {arr[2]} \n')
+    if (sup_str == ''): return 'В этом классе нет учеников'
+    else: return sup_str
 
 def sort_abc(source):
     result = []
+    sup_str = ''
     for i in source:
         result.append(f'{source[i]["surname"]} {source[i]["name"]} id - {i}')
     result.sort()
     for i in result:
-        print(i)
+        sup_str += f'{i} \n'
+    return sup_str
 
+
+# Дополнительный метод для сортировки по классу, работает с переменной, хранящей таблицу
+def sort_class(source):
+    num_class = input('Введите номер класса: ')
+    sup_str =''
+    for i in source:
+        if (source[i]["class"] == str(num_class)):
+            sup_str += (f'{i} {source[i]["surname"]} {source[i]["name"]} - {source[i]["class"]} \n')
+    if (sup_str == ''): return 'В этом классе нет учеников'
+    else: return sup_str
